4287a14cc89c5fcfa2511078c610643250354247
marta <martatruszcz@o2.pl>
2025-07-06 09:29:44 +0000

    code update main.py

--- AMR-DL/scripts/main.py
+++ AMR-DL/scripts/main.py
@@ -4,7 +4,7 @@ import sklearn
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 
-file = open("/scratch/AMR/Variant_GPA_Kmer_uncompressed/Pseudomonas_aeruginosa_train_genotype_vargpakmer.tsv", 'r')
+file = open("/srv/scratch/AMR/Variant_GPA_Kmer_uncompressed/Pseudomonas_aeruginosa_train_genotype_vargpakmer.tsv", 'r')
 
 reader = csv.reader(file, delimiter='\t')
 
@@ -13,7 +13,7 @@ genotype_data = {rows[0]: rows[1:] for rows in reader}
 feature_names = headers[1:]
 
 # Use StringIO to simulate a file object
-file2 = open("/home/user/SMTB2025_AMR/AMR-DL/Data/Phenotype/IR_phenotype/Pseudomonas_aeruginosa/phenotype.txt", 'r')
+file2 = open("/srv/scratch/AMR/IR_phenotype/Pseudomonas_aeruginosa/phenotype.txt", 'r')
 
 reader = csv.reader(file2, delimiter='\t')
 headers = next(reader)
@@ -33,6 +33,66 @@ phenotype_data = ordered_phenotype_data
 
 genotype_array = np.array([list(map(int, genotypes)) for genotypes in genotype_data.values()])
 phenotype_array = np.array([int(phenotypes[antibiotic_index]) for phenotypes in phenotype_data.values()])
+
+train = []
+test = []
+validation = []
+
+train_file = open("/home/marta/SMTB2025_AMR/AMR-DL/Data/Splits/Pseudomonas_aeruginosa/train.txt", 'r')
+test_file = open("/home/marta/SMTB2025_AMR/AMR-DL/Data/Splits/Pseudomonas_aeruginosa/test.txt", 'r')
+validation_file = open("/home/marta/SMTB2025_AMR/AMR-DL/Data/Splits/Pseudomonas_aeruginosa/validation.txt", 'r')
+
+for line in train_file:
+    line = line.strip()
+    train.append(line)
+
+for line in test_file:
+    line = line.strip()
+    train.append(line)
+
+for line in validation_file:
+    line = line.strip()
+    train.append(line) #tu skonczylam
+
+X_train = []
+y_train = []
+X_test = []
+y_test = []
+X_validation = []
+y_validation = []
+
+train_strains_to_be_used = []
+test_strains_to_be_used = []
+validation_strains_to_be_used = []
+
+for train_strain in train:
+    if train_strain in strain_to_index:
+        train_strains_to_be_used.append(train_strain)
+        idx = strain_to_index[train_strain]
+        X_train.append(genotype_array[idx])  # Append the list of genotypes using the index
+        y_train.append(phenotype_array[idx])  # Append the phenotype value using the index
+
+for test_strain in test:
+    if test_strain in strain_to_index:
+        test_strains_to_be_used.append(test_strain)
+        idx = strain_to_index[test_strain]
+        X_test.append(genotype_array[idx])  # Append the list of genotypes using the index
+        y_test.append(phenotype_array[idx])  # Append the phenotype value using the index
+
+for validation_strain in validation:
+    if validation_strain in strain_to_index:
+        validation_strains_to_be_used.append(validation_strain)
+        idx = strain_to_index[validation_strain]
+        X_validation.append(genotype_array[idx])  # Append the list of genotypes using the index
+        y_validation.append(phenotype_array[idx])  # Append the phenotype value using the index
+
+# Convert lists to numpy arrays
+X_train = np.array(X_train, dtype=int)
+y_train = np.array(y_train, dtype=int)
+X_test = np.array(X_test, dtype=int)
+y_test = np.array(y_test, dtype=int)
+X_validation = np.array(X_validation, dtype=int)
+y_validation = np.array(y_validation, dtype=int)
 
 X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(genotype_array, phenotype_array, random_state=42, test_size=float(0.2))
 
@@ -54,3 +114,4 @@ for i in sorted_importances:
         break
     else:
         print(i)
+
