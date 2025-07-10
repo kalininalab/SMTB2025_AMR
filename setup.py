b1c6f04203bc0eba5c40bd778d91fd8a6d99645c
Ilya Senatorov <il.senatorov@protonmail.com>
2025-07-10 11:40:42 +0000

    restructure

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+
+setup(
+    name="smtb_amr",
+    version="0.1.0",
+    author="Ilya Senatorov",
+    author_email="il.senatorov@protonmail.com",
+    packages=["smtb_amr"],
+    install_requires=requirements,
+    python_requires=">=3.11",
+)
